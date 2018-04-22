219d74eb8618b6001a636db47ab675998006bdda
Ronald Gerlach <wordsmith4u@hotmail.com>
2018-04-22 20:00:34 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,2 @@
-# Write your #display_rainbow method here
+display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+  puts "R:[0], O:[1], Y [2], G:[3], B:[4], I:[5], V:[6]}"
