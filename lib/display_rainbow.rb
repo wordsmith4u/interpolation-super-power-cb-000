7c6f27a46d96d6edd7519a935b1c43d9b56ebc6a
Ronald Gerlach <wordsmith4u@hotmail.com>
2018-04-22 20:04:01 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,2 +1,2 @@
-display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-  puts "R:[0], O:[1], Y [2], G:[3], B:[4], I:[5], V:[6]}"
+display_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  puts "R:[0], O:[1], Y [2], G:[3], B:[4], I:[5], V:[6]"
